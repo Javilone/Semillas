9f2db0646e559ff279da3af2c30541d9d7d578f5
Javilone <javilone.blummer@gmail.com>
2024-07-19 13:43:32 +0200

    Añadida semilla con soporte React+Bootstrap+Sass

---
+++ Webpack-Semillas/ES6 React Bootstrap SASS/webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  context: path.resolve(__dirname, "./src"),
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  entry: {
+    app: "./main.jsx",
+    vendorStyles: ["../node_modules/bootstrap/dist/css/bootstrap.css"],
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./index.html",
+      scriptLoading: "blocking",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+  devServer: {},
+};
